7a7b617cc6e16e359afcddfcae856525639e5b39
Ionică Bizău <bizauionica@yahoo.com>
2014-10-18 22:06:01 +0300

    JSDoc comments

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+/**
+ * LazyApi
+ * Defines the new property in the scope, adding the `get` handler.
+ *
+ * @name exports
+ * @function
+ * @param {Object} scope The scope object (default: `this`)
+ * @param {String} name Property (API) name
+ * @param {String} path The path to JavaScript/JSON file.
+ * @return {Object} The object where the property was defined.
+ */
 var LazyApi = module.exports = function (scope, name, path) {
     if (arguments.length < 3) {
         scope = this;
@@ -10,6 +21,18 @@ var LazyApi = module.exports = function (scope, name, path) {
     });
 };
 
-LazyApi.returnHandler = function (path) {
+/**
+ * returnHandler
+ * The default function that will be called when a property is accessed.
+ * This function can be overrided with custom code.
+ *
+ * @name returnHandler
+ * @function
+ * @param {String} path The path to JavaScript/JSON file.
+ * @param {String} name Property (API) name
+ * @param {Object} scope The scope object (default: `this`)
+ * @return {Object} The export object of a JSON/JavaScript file.
+ */
+LazyApi.returnHandler = function (path, name, scope) {
     return require(path);
 };
