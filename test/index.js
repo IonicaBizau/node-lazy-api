0527bb0b66790fab2f7a81aef4e0ae85c7cb3fad
Ionică Bizău <bizauionica@yahoo.com>
2014-12-24 09:18:47 +0200

    Updated test

--- test/index.js
+++ test/index.js
@@ -1,7 +1,29 @@
-var LazyApi = require("../index");
+// Dependencies
+var LazyApi = require("../lib");
 
 var Apis = {};
-LazyApi(Apis, "something", __dirname + "/apis");
-Apis.something.method({foo: "bar"}, function (err, d) {
+
+// Instead of doing
+// Apis.some = require("./some");
+// we do:
+LazyApi(Apis, "some", __dirname + "/some");
+
+// And when we will call this method, the file
+// will be loaded in the RAM.
+Apis.some.method({foo: "bar"}, function (err, d) {
+    console.log(err || d);
+});
+
+// What if we need to run some custom handlers?
+LazyApi.returnHandler = function (path, name, scope) {
+    console.log("Loading " + path);
+    return require(path);
+};
+
+// "Load" (but not in RAM) the another file
+LazyApi(Apis, "another", __dirname + "/another");
+
+// Call the method (this will load the file)
+Apis.another.method({foo: "bar"}, function (err, d) {
     console.log(err || d);
 });
